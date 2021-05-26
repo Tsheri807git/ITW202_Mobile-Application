95b4070709904024603679b155c884fdacf88137
Tsheri807git <12190098.gcit@rub.edu.bt>
2021-05-26 17:15:33 +0600

    todo

--- TODO_20iii/app/src/androidTest/java/gcit/edu/todo_20iii/ExampleInstrumentedTest.java
+++ TODO_20iii/app/src/androidTest/java/gcit/edu/todo_20iii/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.amdroid.todo_4;
+package gcit.edu.todo_20iii;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.amdroid.todo_4", appContext.getPackageName());
+        assertEquals("gcit.edu.todo_20iii", appContext.getPackageName());
     }
 }
